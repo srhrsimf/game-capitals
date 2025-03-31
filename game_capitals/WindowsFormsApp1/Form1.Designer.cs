d200d54eedb820cbed5dd97576bce4ec03c8cdb8
Nikolai <157356672+srhrsimf@users.noreply.github.com>
2025-03-31 18:52:35 +0500

    Add files via upload

---
+++ game_capitals/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(419, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выбери страну, в которой находится город!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(17, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(13, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Город";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(195, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Страна";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(17, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Сгенерировать город";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(333, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Россия",
+            "Китай",
+            "Япония",
+            "США",
+            "Мексика",
+            "Беларусь"});
+            this.comboBox1.Location = new System.Drawing.Point(199, 87);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(93, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button2.Location = new System.Drawing.Point(199, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 46);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Ответить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label5.Location = new System.Drawing.Point(14, 256);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(444, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Работу выполнили:Каримов Айдар, Георгиев Николай ИП-301";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(413, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "0";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label7.Location = new System.Drawing.Point(324, 159);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Очки:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources._151188788863821410471;
+            this.ClientSize = new System.Drawing.Size(490, 301);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(506, 340);
+            this.MinimumSize = new System.Drawing.Size(506, 340);
+            this.Name = "Form1";
+            this.Text = "Игра для малышей :з 我们爱孩子";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ game_capitals/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int sch = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.KeyPress += (sender, e) => e.Handled = true;
+            
+        }
+        
+
+        public void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void textBox2_TextChanged(object sender, EventArgs e)
+        {
+       
+        }
+
+
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            string[] mas_rus = new string[5] { "Екатеринбург", "Омск", "Катайск", "Новоуральск", "Москва" };
+            string[] mas_china = new string[5] { "Шэньчжэнь", "Пекин", "Шанхай", "Чэнду", "Чугучак" };
+            string[] mas_japan = new string[5] { "Токио", "Осака", "Киото", "Сайтама", "Кавасаки" };
+            string[] mas_usa = new string[5] { "Вашингтон", "Нью-Йорк", "Чикаго", "Хьюстон", "Остин" };
+            string[] mas_belarus = new string[5] { "Полоцк", "Минск", "Брест", "Могилёв", "Гродно" };
+            string[] mas_mexico = new string[5] { "Матаморос", "Оахака", "Пуэбла", "Канкун", "Мерида" };
+            string[][] rand_mas = new string[][] {mas_rus, mas_usa, mas_belarus, mas_japan, mas_china, mas_mexico };
+
+            Random rnd = new Random();
+            int i = rnd.Next(0, 6);
+            int j = rnd.Next(0, 5);
+
+            textBox2.Text = rand_mas[i][j].ToString();
+            label4.Text = "";
+            comboBox1.SelectedIndex = -1;
+            
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            string[] mas_rus = new string[5] { "Екатеринбург", "Омск", "Катайск", "Новоуральск", "Москва" };
+            string[] mas_china = new string[5] { "Шэньчжэнь", "Пекин", "Шанхай", "Чэнду", "Чугучак" };
+            string[] mas_japan = new string[5] { "Токио", "Осака", "Киото", "Сайтама", "Кавасаки" };
+            string[] mas_usa = new string[5] { "Вашингтон", "Нью-Йорк", "Чикаго", "Хьюстон", "Остин" };
+            string[] mas_belarus = new string[5] { "Полоцк", "Минск", "Брест", "Могилёв", "Гродно" };
+            string[] mas_mexico = new string[5] { "Матаморос", "Оахака", "Пуэбла", "Канкун", "Мерида" };
+
+            string text = textBox2.Text;
+            string a, b;
+
+            a = textBox2.Text;
+           
+            if (comboBox1.Text == "Китай")
+            {
+                for(int i = 0; i<5; i++)
+                {
+                    b = mas_china[i];
+                    if (a==b)
+                    {
+                        label4.Text = "Правильно";
+                        i = 5;
+                    }
+                    else
+                    {
+                        label4.Text = "Неправильно";
+                    }
+                }
+                Console.WriteLine("a=", a);
+            }
+            else if (comboBox1.Text == "Россия")
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    b = mas_rus[i];
+                    if (a == b)
+                    {
+                        label4.Text = "Правильно";
+                        i = 5;
+                    }
+                    else
+                    {
+                        label4.Text = "Неправильно";
+                    }
+                }
+            }
+            else if (comboBox1.Text == "Япония")
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    b = mas_japan[i];
+                    if (a == b)
+                    {
+                        label4.Text = "Правильно";
+                        i = 5;
+                    }
+                    else
+                    {
+                        label4.Text = "Неправильно";
+                    }
+                }
+            }
+            else if (comboBox1.Text == "США")
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    b = mas_usa[i];
+                    if (a == b)
+                    {
+                        label4.Text = "Правильно";
+                        i = 5;
+                    }
+                    else
+                    {
+                        label4.Text = "Неправильно";
+                    }
+                }
+            }
+            else if (comboBox1.Text == "Мексика")
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    b = mas_mexico[i];
+                    if (a == b)
+                    {
+                        label4.Text = "Правильно";
+                        i = 5;
+                    }
+                    else
+                    {
+                        label4.Text = "Неправильно";
+                    }
+                }
+            }
+            else if (comboBox1.Text == "Беларусь")
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    b = mas_belarus[i];
+                    if (a == b)
+                    {
+                        label4.Text = "Правильно";
+                        i = 5;
+                    }
+                    else
+                    {
+                        label4.Text = "Неправильно";
+                    }
+                }
+            }
+
+            if(textBox2.Text=="")
+            {
+                label4.Text = "";
+                MessageBox.Show("Сгенерируйте город!!!","快来吧!");
+            }
+
+            if (comboBox1.Text == "")
+            {
+                label4.Text = "";
+                MessageBox.Show("Выберите страну!!!","快来吧");
+            }
+
+            if (label4.Text=="Правильно")
+            {
+                sch += 1;
+            }
+
+
+            if (label4.Text == "Неправильно")
+            {
+                sch = 0;
+            }
+
+            label6.Text = sch.ToString();
+            sch = Convert.ToInt32(sch);
+            comboBox1.SelectedIndex = -1;
+            textBox2.Clear();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
